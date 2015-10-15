4e597218650d8df82cd4a809d3ea29f47cd79c4e
saraval09 <saramaneval@Saras-MacBook-Pro.local>
2015-10-15 18:51:41 -0400

    Example Complete
    Example Complete

---
+++ Maneval_Sara_WIA_Presentation/Gridster/js/main.js
@@ -0,0 +1,9 @@
+var gridster;
+
+$(function() {
+    gridster = $(".gridster > ul").gridster({
+        widget_margins: [10, 10],
+        widget_base_dimensions: [140, 140],
+        min_cols: 6
+    }).data('gridster');
+});
